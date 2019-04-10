509e1f81bb789b7ecafe3d8550519d26c58f0421
eazrik <eazrik@gmail.com>
2019-04-10 15:56:44 +0800

    Refs #master - inheritance continue and abstract class

--- Shape.php
+++ Shape.php
@@ -1,7 +1,20 @@
 <?php
 
-class Shape
+abstract class Shape
 {
+    protected $color;
+
+    public function __construct($color = 'red')
+    {
+        $this->color = $color;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    abstract protected function getArea();
 }
 
 class Square extends Shape
@@ -14,4 +27,26 @@ class Square extends Shape
     }
 }
 
-echo (new Square)->getArea();
+class Triangle extends Shape
+{
+    protected $base = 4;
+
+    protected $height = 7;
+
+    public function getArea()
+    {
+        return .5 * $this->base * $this->height;
+    }
+}
+
+class Circle extends Shape
+{
+    protected $radius = 5;
+
+    public function getArea()
+    {
+        return M_PI * pow($this->radius, 2);
+    }
+}
+
+echo (new Circle)->getArea();
