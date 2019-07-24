a024315e5c0125851860e926a8038359064b7a6e
eazrik <eazrik@gmail.com>
2019-07-24 13:08:19 +0800

    refs #develop composer check

--- src/Person.php
+++ src/Person.php
@@ -2,10 +2,21 @@
 
 namespace Acme;
 
+/**
+ * Class Person
+ * @package Acme
+ */
 class Person
 {
+    /**
+     * @var string $name
+     */
     protected $name;
 
+    /**
+     * Person constructor.
+     * @param string $name Person name
+     */
     public function __construct($name)
     {
         $this->name = $name;
