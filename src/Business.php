f7b62123906aa24fed7fe3496c9cc8734c211736
eazrik <eazrik@gmail.com>
2019-07-25 17:07:51 +0800

    refs #feature_php_doc create custom ruleset xml

--- src/Business.php
+++ src/Business.php
@@ -6,27 +6,16 @@ class Business
 {
     protected $staff;
 
-    /**
-     * Business constructor.
-     *
-     * @param Staff $staff staff
-     */
     public function __construct(Staff $staff)
     {
         $this->staff = $staff;
     }
 
-    /**
-     * @param Person $person person
-     */
     public function hire(Person $person)
     {
         $this->staff->add($person);
     }
 
-    /**
-     * @return array
-     */
     public function getStaffMembers()
     {
         return $this->staff->members();
