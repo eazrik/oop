e18458ec5ce2352db0a305cea1ffe1ecb7272514
eazrik dazilie <eazrik@gmail.com>
2020-02-19 18:33:29 +0800

    test ci

--- Messages.php
+++ Messages.php
@@ -15,19 +15,3 @@ $acme->hire(new Person('Jane Doe'));
 echo '<pre>';
 var_dump($acme->getStaffMembers());
 echo '</pre>';
-
-echo 'feature test';
-echo 'feature test 2';
-echo 'feature test 3';
-
-echo 'feature messages';
-echo 'feature messages 2';
-echo 'feature messages 3';
-
-
-echo 'feature 58888 1';
-echo 'feature 58888 2';
-echo 'feature 58888 3';
-
-
-echo '100';
